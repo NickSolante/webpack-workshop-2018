22ca833bf8f59d9389584dec040ff87e6d997222
NickSolante <41457938+NickSolante@users.noreply.github.com>
2022-10-21 08:51:16 +1100

    webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import nav from "./nav";
-import { top, bottom } from "./footer";
-import makeButton from "./button";
-import { makeColorStyle } from "./button-styles";
+import nav from "./nav"
+import { top, bottom, footer } from "./footer"
+import makeButton from "./button"
+import { makeColorStyle } from "./button-styles"
 
-const button = makeButton('Yay a button!!!!');
-document.body.appendChild(button);
+const button = makeButton("Yay a button!!!!")
+button.style = makeColorStyle("cyan")
+document.body.appendChild(button)
+document.body.appendChild(footer)
