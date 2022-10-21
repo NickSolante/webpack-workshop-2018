ed31299dc4f28e95f7c42ff652a580af3e37ca43
NickSolante <41457938+NickSolante@users.noreply.github.com>
2022-10-21 20:24:47 +1100

    create-best-practice-css-loading

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import nav from "./nav";
-import { footer } from "./footer";
-import makeButton from "./button";
-import { makeColorStyle } from "./button-styles";
-
-const button = makeButton("Yay! A Button!");
-button.style = makeColorStyle("cyan");
-document.body.appendChild(button);
-document.body.appendChild(footer);
+import nav from "./nav"
+import { footer } from "./footer"
+import makeButton from "./button"
+import { makeColorStyle } from "./button-styles"
+import "./footer.css"
+const button = makeButton("Yay! A Button!")
+button.style = makeColorStyle("cyan")
+document.body.appendChild(button)
+document.body.appendChild(footer)
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
-const webpack = require("webpack")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const webpack = require("webpack")
 const webpackMerge = require("webpack-merge")
-
 const modeConfig = (env) => require(`./build-utils/webpack.${env}`)(env)
 
 module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
 	return webpackMerge(
 		{
 			mode,
-			output: {
-				filename: "bundle.js",
+			module: {
+				rules: [
+					{
+						test: /\.js$/,
+						exclude: /node_modules/,
+						use: ["babel-loader"],
+					},
+				],
 			},
 			plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
 		},
