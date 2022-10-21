b527cb16986a231b8b7b58921bb0d3454a3b8141
NickSolante <41457938+NickSolante@users.noreply.github.com>
2022-10-21 18:12:39 +1100

    first

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpackMerge = require("webpack-merge");
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const webpackMerge = require("webpack-merge")
 
-const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
+const modeConfig = (env) => require(`./build-utils/webpack.${env}`)(env)
 
 module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
-  return webpackMerge(
-    {
-      mode,
-      output: {
-        filename: "bundle.js"
-      },
-      plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
-    },
-    modeConfig(mode)
-  );
-};
+	return webpackMerge(
+		{
+			mode,
+			output: {
+				filename: "bundle.js",
+			},
+			plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
+		},
+		modeConfig(mode)
+	)
+}
